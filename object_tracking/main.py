34c7f3f08d350b7ec6dffaf0371d2612eb62fbd9
Zofia Kubrak <zofiakubrak@gmail.com>
2022-03-21 19:59:24 +0100

    code cleaning and deleting unused packages

--- object_tracking/main.py
+++ object_tracking/main.py
@@ -1,4 +1,5 @@
 import cv2
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 if not cap.isOpened():
@@ -17,17 +18,31 @@ while True:
 
     if success:
         (x, y, w, h) = [int(v) for v in bbox]
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2, 1)
-        cv2.putText(img, "Object tracking", (100, 100),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2, 1)
+        cv2.putText(
+            img,
+            "Object tracking",
+            (100, 100),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 0, 255),
+            2,
+        )
     else:
-        cv2.putText(img, "Object lost", (100, 100),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+        cv2.putText(
+            img,
+            "Object lost",
+            (100, 100),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 0, 255),
+            2,
+        )
 
     cv2.imshow("Input", img)
 
     # press q to exit
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
